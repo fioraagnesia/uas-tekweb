922540625a5dfdf3695c58f16bf31f1a8ca15e4c
fioraagnesia <142294637+fioraagnesia@users.noreply.github.com>
2024-12-16 00:20:21 +0700

    Delete TekwebLatihanUAS/UAS2021 directory

--- TekwebLatihanUAS/UAS2021/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = new mysqli("localhost", "root", "", "tekweb_latihan");
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    };
-?>
--- TekwebLatihanUAS/UAS2021/saveSession.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['id'])) {
-    $_SESSION['id_transaksi'] = $_POST['id'];
-    echo 'ID Transaksi Disimpan';
-}
-?>
